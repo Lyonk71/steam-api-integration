7866d267147720a78c99951d5abb107b61923e43
Kenny Day <kenny@rushdownstudio.com>
2022-05-17 11:28:45 -0400

    initial commit

---
+++ util/config.go
@@ -0,0 +1,26 @@
+// Package util handles utility functions like loading configuration
+package util
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	Port       string `mapstructure:"SERVER_PORT"`
+	APIKey     string `mapstructure:"STEAM_API_KEY"`
+	SteamAppID string `mapstructure:"STEAM_APP_ID"`
+	Env        string `mapstructure:"ENV"`
+}
+
+func LoadConfig() (config Config, err error) {
+	viper.AddConfigPath("./")
+	viper.SetConfigName("config")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
